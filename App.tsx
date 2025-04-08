3f077d8ad99089197b2d43a23a288d51c8c61ab1
guillaumehcht <guillaumehuchet3@gmail.com>
2025-04-08 10:58:42 +0200

    Add expo-maps integration and update app configuration
    - Added support for Apple Maps and Google Maps in App.tsx with marker data.
    - Updated app.json to include bundle identifiers for iOS and Android.
    - Modified package.json and package-lock.json to include expo-maps dependency.
    - Updated .gitignore to exclude ios/ and android/ directories.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,73 @@
+import { AppleMaps, GoogleMaps } from 'expo-maps';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Platform, StyleSheet, Text, View } from 'react-native';
+
+type Marker = {
+	coordinates: {
+		latitude: number;
+		longitude: number;
+	};
+	systemImage: string;
+	tintColor: string;
+	title: string;
+};
+
+const MARKER_DATA: Marker[] = [
+	{
+		coordinates: {
+			latitude: 44.608543,
+			longitude: -1.094672,
+		},
+		systemImage: 'mappin.circle.fill',
+		tintColor: 'blue',
+		title: 'Plage des Arbousiers',
+	},
+	{
+		coordinates: {
+			latitude: 44.604215,
+			longitude: -1.091453,
+		},
+		systemImage: 'fork.knife.circle.fill',
+		tintColor: 'orange',
+		title: 'Café du Port',
+	},
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			{Platform.OS === 'ios' ? (
+				<AppleMaps.View
+					style={{ flex: 1 }}
+					cameraPosition={{
+						coordinates: {
+							latitude: 44.60414166575967,
+							longitude: -1.091734245661981,
+						},
+						zoom: 14.5,
+					}}
+					markers={MARKER_DATA.map((marker: Marker) => ({
+						coordinates: marker.coordinates,
+						systemImage: marker.systemImage,
+						tintColor: marker.tintColor,
+						title: marker.title,
+					}))}
+					onMarkerClick={(marker) => {
+						console.log('marker', marker);
+						Alert.alert('Marker clicked', marker.title);
+					}}
+				/>
+			) : (
+				<GoogleMaps.View style={{ flex: 1 }} />
+			)}
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
 });
